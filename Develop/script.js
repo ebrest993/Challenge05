1cf13b442f8090050b24495cda1e5b2efcb306a1
Elliott Brest <elliott.brest@gmail.com>
2023-07-16 17:09:01 -0500

    added starting functionality to save buttons

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,22 @@
-let saveBtn;
+let saveBtn = $(".saveBtn");
 let addedToDo = [];
 let todayDate = dayjs();
-let displayDate;
+let timeSlot = $("time-block");
 
-$("#currentDay").text(todayDate.format("MMM D, YYYY"));
-console.log();
+$("#currentDay").text(todayDate.format("MMM, D YYYY"));
+console.log(saveBtn);
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  $(saveBtn).on("click", function () {
+
+  console.log("beep-boop");
+  // need a jQuery function to send the entered event to local storage
+  // also need a function to determine css styling and when the event occurs
+  // jQuery version of localStorage.setItem to store event
+  // jQuery version of localStorage.getItem to keep event displayed on calendar
+})
 });
+
+
--- README.md
+++ README.md
@@ -14,9 +14,6 @@
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-
 
 # 05 Third-Party APIs: Work Day Scheduler
 
