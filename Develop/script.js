ae5317a921e0cade8eea10f0f1473ca7d565aa66
Elliott Brest <elliott.brest@gmail.com>
2023-07-17 15:09:30 -0500

    events stored in local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,22 +1,30 @@
+$(function () {
 let saveBtn = $(".saveBtn");
-let addedToDo = [];
 let todayDate = dayjs();
-let timeSlot = $("time-block");
+let timeSlot = $(".description");
+let calendForm = $(".time-block")
+let toDo = [];
+  
+let dayEvent = localStorage.getItem("to-dos");
 
-$("#currentDay").text(todayDate.format("MMM, D YYYY"));
-console.log(saveBtn);
+  $("#currentDay").text(todayDate.format("MMM, D YYYY"));
+
+  $(timeSlot).text(dayEvent);
+
+  $(function () {
+    $(saveBtn).on("click", function () {
+      $.each(timeSlot, function () {
+        toDo.push($(this).val());
+        localStorage.setItem("to-dos", toDo);
+      });
+    })
+  });
+
+
+
+
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  $(saveBtn).on("click", function () {
-
-  console.log("beep-boop");
-  // need a jQuery function to send the entered event to local storage
-  // also need a function to determine css styling and when the event occurs
-  // jQuery version of localStorage.setItem to store event
-  // jQuery version of localStorage.getItem to keep event displayed on calendar
 })
-});
-
-
