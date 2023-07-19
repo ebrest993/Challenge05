cd0c1f92b5a528ae84ad1e52a245ebd9c12a6904
Elliott Brest <elliott.brest@gmail.com>
2023-07-19 15:38:50 -0500

    assign number value to each time slot and worked out an if else statement and for loop

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,21 @@
-$(function () {
+$(function (event) {
   let saveBtn = $(".saveBtn");
+  let currentHour = dayjs().format("HH");
   let todayDate = dayjs();
-  let timeSlot = $(".description");
-  let calendForm = $(".container-lg")
-  let toDo = [];
-  let timeWindow = document.querySelector("time");
+  let timeSlot;
+  let calendForm = $(".time-block");
+  let xyz = [
+    "09",
+    "10",
+    "11",
+    "12",
+    "13",
+    "14",
+    "15",
+    "16",
+    "17"
+  ];
   
-  console.log(timeWindow);
   $("#currentDay").text(todayDate.format("MMM, D YYYY"));
 
   $(function () {
@@ -14,14 +23,28 @@ $(function () {
       let value = $(this).siblings(".description").val();
       let time = $(this).parent().attr("id");
 
-      localStorage.setItem(time, value); 
+      localStorage.setItem(time, value);
+        console.log(time); 
+        console.log(value);
       });
     })
 
-  let hourJS = $(timeSlot).val($(dayjs("9:00")));
-  console.log(hourJS);
-  
+    for (i=0; i < xyz.length; i++) {
+      timeSlot = xyz[i];
 
+      if (timeSlot === currentHour) {
+        calendForm.addClass("present");
+        console.log("present class");
+      } 
+      if (timeSlot < currentHour) {
+        calendForm.addClass("past");
+        console.log("past class");        
+     } 
+      if (timeSlot > currentHour) {
+        calendForm.addClass("future");
+        console.log("future class");        
+     };}
+    
   $('#hour-9 .description').val(localStorage.getItem('hour-9'));
   $('#hour-10 .description').val(localStorage.getItem('hour-10'));
   $('#hour-11 .description').val(localStorage.getItem('hour-11'));
@@ -31,11 +54,4 @@ $(function () {
   $('#hour-15 .description').val(localStorage.getItem('hour-15'));
   $('#hour-16 .description').val(localStorage.getItem('hour-16'));
   $('#hour-17 .description').val(localStorage.getItem('hour-17'));
-  });
-
-
-
-
-
-
-
+})
